b36c20cce4c33eb7a796e95f4aecf18b65fd3a02
AzizDevX <azizkammoun1919@gmail.com>
2025-08-08 22:49:47 +0100

    Routers Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import AuthPage from "./components/auth/auth";
 import { Frontend_Admin_Url } from "./config/AdminUrl.json";
+import Dashboard from "./components/AdminDashboard/main/Dashboard_Restructured";
+import DeniedPage from "./components/AccesDenied/DeniedPage";
 const adminUrl = "/" + Frontend_Admin_Url;
+const adminDashboard_Url = adminUrl + "/dashboard";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path={adminUrl} element={<AuthPage />} />
+        <Route path={adminDashboard_Url} element={<Dashboard />} />
+        <Route path="/denied" element={<DeniedPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
