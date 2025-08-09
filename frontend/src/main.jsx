4e154799b7d63fe0f7363d40808b5967a184f88f
AzizDevX <azizkammoun1919@gmail.com>
2025-08-09 01:24:26 +0100

    Added: 404 Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import AuthPage from "./components/auth/auth";
 import { Frontend_Admin_Url } from "./config/AdminUrl.json";
 import Dashboard from "./components/AdminDashboard/main/Dashboard_Restructured";
 import DeniedPage from "./components/AccesDenied/DeniedPage";
+import Page404 from "./components/404/404page";
 const adminUrl = "/" + Frontend_Admin_Url;
 const adminDashboard_Url = adminUrl + "/dashboard";
 
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")).render(
         <Route path={adminUrl} element={<AuthPage />} />
         <Route path={adminDashboard_Url} element={<Dashboard />} />
         <Route path="/denied" element={<DeniedPage />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
