5940a6c93671506da7de20c0599b10160a97e7a8
AzizDevX <azizkammoun1919@gmail.com>
2025-08-10 01:32:40 +0100

    Added : FooterSocialLinksShema

--- backend/models/FooterSocialLinksShema.js
+++ backend/models/FooterSocialLinksShema.js
@@ -2,6 +2,17 @@ import mongoose from "mongoose";
 const FooterSocialLinks = mongoose.Schema({
   SocialIcon: {
     type: String,
+    enum: [
+      "Facebook",
+      "Twitter",
+      "Instagram",
+      "LinkedIn",
+      "GitHub",
+      "YouTube",
+      "Mail",
+      "Twitch",
+      "Globe",
+    ],
     required: true,
   },
   SocialLink: {
